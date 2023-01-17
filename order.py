20aab99da1de032fff34676232a725f583bc9d28
albert199577 <mso729049@gmail.com>
2023-01-17 14:40:42 +0800

    feat: add variable control order

--- order.py
+++ order.py
@@ -1,12 +1,46 @@
 from selenium import webdriver
 
+import os, sys
+import datetime
+
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# from order import order
+ac = input("Please Enter Your Account :")
+pw = input("Please Your Password :")
+bp = input("Please Enter your Booking peoples :")
+eat_time_num = int(input("選擇您的餐次 [1]午餐 [2]下午餐 [3]晚餐 "))
+store_num = int(input("選擇您的店別 [1]微風店 [2]新莊店 "))
+order_date = input("輸入您要訂餐的日期 ex:2023-01-24 ")
+
+now = datetime.datetime.now()
+year = order_date[0:4]
+int_month = int(order_date[5:7])
+month = order_date[5:7]
+
+day = order_date[8:10]
+
+ch_month = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+ch_month = ch_month[int_month - 1]
+
+if month == now.strftime("%m"):
+    this_month = True
+else:
+    this_month = False
+
+eat_time_arr = ["wk-type-lunch", "wk-type-afternoon-tea", "wk-type-dinner"]
+eat_time = eat_time_arr[eat_time_num - 1]
+
+store_num_arr = ['li[rel="微風店"]', 'li[rel="新莊店"]']
+store = store_num_arr[store_num - 1]
+
+date = 'ul.days > li.notfull > span.notfull[data-col-date="' + order_date + '"]'
+order_date_field = '.dayContainer > span[aria-label="{ch_month} {day}, {year}"]'.format(ch_month=ch_month, day=day, year=year)
+
+# os._exit(0)
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option("detach", True) # 不自動關閉視窗
@@ -27,12 +61,12 @@ except:
 WebDriverWait(driver, 3000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="header"]/div[2]/div/ul/li[1]'))).click()
 
 # 填入會員帳號
-ac = WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="form-login-header"]/ul/li[1]/input')))
-ac.send_keys('')
+ac_input = WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="form-login-header"]/ul/li[1]/input')))
+ac_input.send_keys(ac)
 
 # 填入會員密碼
-pw = WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="form-login-header"]/ul/li[2]/input')))
-pw.send_keys('')
+pw_input = WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="form-login-header"]/ul/li[2]/input')))
+pw_input.send_keys(pw)
 
 # 點擊登入
 WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="form-login-header"]/ul/li[4]/button'))).click()
@@ -47,41 +81,29 @@ try:
 except:
     print("alert does not Exist in page")
 
-
 i = 0
-while 1:
+while i < 1:
     i += 1
-    print (i)
     try:
-        # 關閉注意事項
-        # try:
-        #     WebDriverWait(driver, 2000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="modal-news"]/div/div/div/button'))).click()
-        # except:
-        #     print("close error")
-
         # 訂位人數
         book_people = WebDriverWait(driver, 2000).until(EC.visibility_of_element_located((By.ID, "book_people")))
         # book_people = driver.find_element(By.ID, 'book_people')
         # 填上訂位人數
-        book_people.send_keys('2')
+        book_people.send_keys(bp)
 
         # 點擊日期
-        # driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div/ul/li[3]/div/input').click()
-        date = driver.find_element(By.CSS_SELECTOR, ".flatpickr.flatpickr-input").click() 
-        # date.send_keys('2023-02-15')
+        driver.find_element(By.CSS_SELECTOR, '.flatpickr.flatpickr-input').click()
 
         # 點擊下個月
-        driver.find_element(By.CLASS_NAME, 'flatpickr-next-month').click()
+        if this_month == False:
+            driver.find_element(By.CLASS_NAME, 'flatpickr-next-month').click()
 
         # 點擊下個月最後一天
-        # driver.find_element(By.XPATH, '/html/body/div[14]/div[2]/div/div[2]/div/span[18]').click()
-        driver.find_element(By.CSS_SELECTOR, '.dayContainer > span[aria-label="一月 17, 2023"]').click()
+        WebDriverWait(driver, 2000).until(EC.visibility_of_element_located((By.CSS_SELECTOR, order_date_field))).click()
 
-        # 點擊晚餐
+        # 點擊餐別
         # CSS_SELECTOR
-        # type = ["wk-type-lunch", "wk-type-afternoon-tea", "wk-type-dinner"]
-        eating_time = 'wk-type-afternoon-tea'
-        driver.find_element(By.CLASS_NAME, eating_time).click()
+        driver.find_element(By.CLASS_NAME, eat_time).click()
 
         js = "document.body.scrollTo(0, 1000);"
 
@@ -89,12 +111,9 @@ while 1:
 
         driver.find_element(By.ID, 'select_store').click()
 
-        # 點擊晚餐  
+        # 點擊店別
         # CSS_SELECTOR
-        # type = ['"rel="全部分店"', "li[rel='微風店']", "li[rel='新莊店']"]
-        select_store = "li[rel='微風店']"
-        WebDriverWait(driver, 1000).until(EC.visibility_of_element_located((By.CSS_SELECTOR, select_store))).click()
-        # WebDriverWait(driver, 100).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div/ul/li[5]/div/div/ul/li[2]'))).click()
+        WebDriverWait(driver, 2000).until(EC.visibility_of_element_located((By.CSS_SELECTOR, store))).click()
 
         # notfull CSS_SELECTOR calendar-li notfull active
         # <li class="calendar-li notfull active">
@@ -107,13 +126,15 @@ while 1:
         #         </span>
         #     </span>
         # </li>
-        WebDriverWait(driver, 1, 0.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'ul.days > li.active.notfull'))).click()
-        # driver.close()
+
+        WebDriverWait(driver, 1, 2).until(EC.visibility_of_element_located((By.CSS_SELECTOR, date))).click()
         print('Can order')
         break;
     except:
         print('Can\'t order')
-        driver.refresh()
+        # driver.refresh()
+        # driver.close()
+        os._exit(0)
 
 
 
